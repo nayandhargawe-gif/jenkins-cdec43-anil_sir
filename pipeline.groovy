5f04248801f21c7d0665c210c19ab6c41e8e8de5
Anil Bamnote <anilbamnote21@gmail.com>
2025-10-29 10:09:08 +0530

    pipeline-stages-added

---
+++ pipeline.groovy
@@ -0,0 +1,59 @@
+pipeline {
+    agent any
+    stages {
+        stage('git_checkout') {
+            steps {
+                echo "pull-sucess"
+            }
+        }
+        stage('build-stage') {
+            steps {
+               
+                echo "build-sucess" 
+            }
+        }
+        stage('test-stage') {
+            steps {
+               
+                echo "test-sucess" 
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploy-sucess"
+            }
+        }
+    }
+}
+
+
+---
+
+
+pipeline {
+    agent any
+    stages {
+        stage('git_checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+        stage('build-stage') {
+            steps {
+               
+                echo "build-sucess" 
+            }
+        }
+        stage('test-stage') {
+            steps {
+               
+                echo "test-sucess" 
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploy-sucess"
+            }
+        }
+    }
+}
