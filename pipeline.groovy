29aa58a716553d03ae62d1243f6ee18100f11cef
nayandhargawe-gif <dhargawenayan@gmail.com>
2025-11-06 16:06:26 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -49,8 +49,10 @@ pipeline {
             //    withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
             //         sh '/opt/maven/bin/mvn sonar:sonar' 
             // }
-                // sh '/opt/maven/bin/mvn sonar:sonar  -Dsonar.projectKey=studentapp -Dsonar.host.url=http://172.31.22.154:9000 -Dsonar.login=05d72f8340ad25c99e810ea637aa4eb0f7c87745'
-                  sh '''/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp1 -Dsonar.host.url=http://172.31.11.57:9000 -Dsonar.login=c7d0475cab0ac0bc8a86058578e0e85ea1c14b86'''
+              withSonarQubeEnv(installationName: 'sonar', credentialsId: 'cred-sonar') {
+    // some block
+            }
+                  // sh '''/opt/maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp1 -Dsonar.host.url=http://172.31.11.57:9000 -Dsonar.login=c7d0475cab0ac0bc8a86058578e0e85ea1c14b86'''
         }
         }
         //  stage('Quality-gate') {
